518846991df55d40002b96c39ca08923b23f0fdf
italo <contato.italo1020@gmail.com>
2020-02-14 16:02:04 -0300

    ajuste no teste do aluno

--- src/tests/testsUnitarios/TestAprovacaoAluno.java
+++ src/tests/testsUnitarios/TestAprovacaoAluno.java
@@ -11,8 +11,8 @@ import model.Aluno;
 public class TestAprovacaoAluno {
 	private Aluno aluno1, aluno2, aluno3;
 
-	@Before
-	public void setUp() {
+	@Test
+	public void testarAprovacaoAluno1() {
 		aluno1 = new Aluno();
 		aluno1.setNome("italo");
 		aluno1.setFaltas(3);
@@ -21,6 +21,12 @@ public class TestAprovacaoAluno {
 		aluno1.adicionarNota(8.5);
 		aluno1.adicionarNota(10.0);
 		
+		assertEquals("aprovado", aluno1.verificarAprovacao());
+		
+	}
+	
+	@Test
+	public void testarAprovacaoAluno2() {
 		aluno2 = new Aluno();
 		aluno2.setNome("maria");
 		aluno2.setFaltas(20);
@@ -29,6 +35,12 @@ public class TestAprovacaoAluno {
 		aluno2.adicionarNota(4.5);
 		aluno2.adicionarNota(4.5);
 		
+		assertEquals("reprovado", aluno2.verificarAprovacao());
+		
+	}
+	
+	@Test
+	public void testarQuantidadeFaltas() {
 		aluno3 = new Aluno();
 		aluno3.setNome("josé");
 		aluno3.setFaltas(0);
@@ -36,22 +48,6 @@ public class TestAprovacaoAluno {
 		aluno3.adicionarNota(10.0);
 		aluno3.adicionarNota(10.0);
 		aluno3.adicionarNota(10.0);
-	}
-
-	@Test
-	public void testarAprovacaoAluno1() {
-		assertEquals("aprovado", aluno1.verificarAprovacao());
-		
-	}
-	
-	@Test
-	public void testarAprovacaoAluno2() {
-		assertEquals("reprovado", aluno2.verificarAprovacao());
-		
-	}
-	
-	@Test
-	public void testarQuantidadeFaltas() {
 		assertEquals("aprovado", aluno3.verificarAprovacao());
 	}
 	
